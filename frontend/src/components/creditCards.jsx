3e73cd2138624ea47269f5210493c0d6be9835e2
Adnan <adnan.shaik@technoidentity.com>
2025-03-15 14:30:07 +0530

    changed links in salesCards and creditCards

--- frontend/src/components/creditCards.jsx
+++ frontend/src/components/creditCards.jsx
@@ -4,7 +4,7 @@ import LoadingSpinner from './loadingSpinner';
 import { useAuth } from '../context/AuthContext';
 import { io } from "socket.io-client";
 
-const socket = io("https://vegetable-retail-store-backend.vercel.app", {
+const socket = io("https://vegetable-retail-store-dashboard.vercel.app", {
   transports: ["websocket", "polling"],
   reconnection: true,
   reconnectionAttempts: 10,
